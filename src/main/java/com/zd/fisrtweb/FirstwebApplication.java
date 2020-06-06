205804cb082be9f4a67e0934cc3c8a0780c8ba55
d.zhang <zd19930112@gmail.com>
2020-06-06 23:22:20 +0900

    登録画面

---
+++ src/main/java/com/zd/fisrtweb/FirstwebApplication.java
@@ -0,0 +1,17 @@
+package com.zd.fisrtweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstwebApplication.class, args);
+    }
+
+    private String startTime() {
+        return "2020/06/06";
+    }
+
+}
